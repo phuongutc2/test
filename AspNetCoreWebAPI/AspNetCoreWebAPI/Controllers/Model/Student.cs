8fd123a1949ddb2dd214f4e78f77a95bec324d3b
Phuong <pin421999@gmail.com>
2020-07-07 21:28:20 +0700

    CRUD

---
+++ AspNetCoreWebAPI/AspNetCoreWebAPI/Controllers/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebAPI.Controllers.Model
+{
+    public class Student
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public int roll { get; set; } = 0;
+
+
+    }
+}
---
+++ AspNetCoreWebAPI/AspNetCoreWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreWebAPI.Controllers.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        List<Student> _oStudents = new List<Student>()
+        {
+            new Student(){Id= 1, Name="Boy",roll=121 },
+            new Student(){Id= 2, Name="Boy2",roll=122 },
+            new Student(){Id= 3, Name="Boy3",roll=123 },
+            new Student(){Id= 4, Name="Boy4",roll=124 },
+            new Student(){Id= 5, Name="Boy5",roll=125 },
+
+        };
+        [HttpGet]
+        public IActionResult Gets()
+        {
+            if (_oStudents.Count == 0)
+            {
+                return NotFound("No list found");
+            }
+            return Ok(_oStudents);
+        }
+        [HttpGet("GetStudent")]
+        public IActionResult Get(int id)
+        {
+            var oStudent = _oStudents.SingleOrDefault(x => x.Id == id);
+            if (oStudent == null)
+            {
+                return NotFound("No Student found.");
+            }
+            return Ok(oStudent);
+        }
+
+        [HttpPost]
+        public IActionResult Save(Student oStudent)
+        {
+            _oStudents.Add(oStudent);
+            if (_oStudents.Count == 0)
+            {
+                return NotFound("No list found.");
+            }
+            return Ok(_oStudents);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var oStudent = _oStudents.SingleOrDefault(x => x.Id == id);
+            if (oStudent == null)
+            {
+                return NotFound("No Student found.");
+            }
+            _oStudents.Remove(oStudent);
+
+            if (_oStudents.Count == 0)
+            {
+                return NotFound("No list found.");
+            }
+            return Ok(_oStudents);
+        }
+       
+        
+            
+    }
+}
